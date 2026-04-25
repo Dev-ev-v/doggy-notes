7b092af25084db9c5f13ecbd887e0659d4ea4151
Dev-ev-v <jogos.testedostestes@gmail.com>
2026-04-25 18:42:51 +0000

    Release real v0.9.0

--- src/my_notes/cli/main.py
+++ src/my_notes/cli/main.py
@@ -1,5 +1,12 @@
 import argparse
-from my_notes.core.services import create_note, get_all_notes, delete_note, search_note
+from pathlib import Path
+from my_notes.infra.serializer import NoteSerializer
+from my_notes.infra.storage import NoteStorage
+from my_notes.json.repository import NoteRepository
+from my_notes.application.service import NoteService
+from my_notes.cli.presenters import NotePrintMessages
+from my_notes.domain.note import Note
+from datetime import datetime
 
 def main():
     parser = argparse.ArgumentParser(description="CLI de notas")
@@ -15,35 +22,39 @@ def main():
     subparsers.add_parser("list")
 
     delete_parser = subparsers.add_parser("delete")
-    delete_parser.add_argument("index", nargs="?", type=int)
+    delete_parser.add_argument("index", nargs="*", type=int)
     delete_parser.add_argument("--all", action="store_true")
 
     search_parser = subparsers.add_parser("search")
     search_parser.add_argument("term")
 
     args = parser.parse_args()
+    
+    directory = Path.home() / 'notes'
+    directory.mkdir(parents=True, exist_ok=True)
+    repo = NoteRepository(NoteStorage(), NoteSerializer(), directory)
+    service = NoteService(repo)
+    print_system = NotePrintMessages()
 
     if args.command == "add":
-        create_note(args.content, args.title, args.description, args.tags)
+        note = Note(
+        content=args.content,
+        title=args.title,
+        description=args.description,
+        tags=args.tags,
+        date=datetime.now())
+        
+        service.create_note(note)
 
     elif args.command == "list":
-        notes = get_all_notes()
-        for i, note in enumerate(notes, 1):
-            print(f"{i}. {note}")
+        notes = service.get_all_notes()
+        print_system.print_notes(notes)
 
     elif args.command == "delete":
-        if not args.all:
-        	if args.index is None or index <= 0:
-        		print("Indice inválido")
-        		return    
-        delete_note(args.index, args.all)
-
-    elif args.command == "search":
-        results = search_note(args.term)
-        if results:
-        	print(f"Resultados encontrados: {results}")
-        else:	
-        	print("Nenhum resultado foi encontrado")
+        files = service.get_note_by_index(args.index, args.all)
+        can_delete = print_system.get_confirmation(files)
+        if can_delete:
+        	service.delete_note(files)
    
     else:
         parser.print_help()
